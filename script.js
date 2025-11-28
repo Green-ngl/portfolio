8b5470d4d8cae3e3787e4b6ac2561369d43a9c5a
Miguel Ponce <amponce@mcm.edu.ph>
2025-11-28 20:39:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+const buttons = document.querySelectorAll(".theme-btn");
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme) {
+  document.documentElement.setAttribute("data-theme", savedTheme);
+}
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const theme = btn.getAttribute("data-theme");
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  });
+});
